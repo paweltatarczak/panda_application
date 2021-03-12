ed5975e1c3807e398b58ca9749ea40e6635e6c08
pawel <pawel@pawel.pl>
2021-03-12 17:08:53 +0000

    pipelineJenkins

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -18,13 +18,13 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
 
     private static WebDriver driver;
 
-    @LocalServerPort
-    private int port;
+    //@LocalServerPort
+    //private int port;
 
     @BeforeEach
     public void startup() throws InterruptedException, MalformedURLException {
@@ -34,7 +34,7 @@ public class PandaApplicationSeleniumTest {
         driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
 
         // Pamiętaj, że aplikacja Spring musi działać!
-        driver.get(String.format("http://ubuntuslave:%s", port));
+        driver.get(String.format("http://ubuntuslave:%s", 8080));
 
         //Czekamy 2 sekundy
         Thread.sleep(2000);
